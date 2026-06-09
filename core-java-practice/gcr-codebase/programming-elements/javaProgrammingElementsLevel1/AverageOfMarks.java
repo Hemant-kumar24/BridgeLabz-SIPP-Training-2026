30ab3d9e2901d62bb2369dfa8b2e958e68e4bc45
Hemant-Kumar <hemantkushwaha2405@gmail.com>
2026-06-09 10:59:33 +0530

    [Hemant] Add: Level 1 programming solutions

---
+++ core-java-practice/gcr-codebase/programming-elements/javaProgrammingElementsLevel1/AverageOfMarks.java
@@ -0,0 +1,12 @@
+package javaProgrammingElementsLevel1;
+import java.util.*;
+public class AverageOfMarks {
+    public static void main(String[] args) {
+    int maths=94;
+    int physics=95;
+    int chemistry=96;
+    double AverageOfMarks=(maths+physics+chemistry)/3;
+    System.out.println("Sam’s average mark in PCM is "+AverageOfMarks);
+    }
+
+}
---
+++ core-java-practice/gcr-codebase/programming-elements/javaProgrammingElementsLevel1/SideOfSquare.java
@@ -0,0 +1,16 @@
+package javaProgrammingElementsLevel1;
+
+import java.util.Scanner;
+
+public class SideOfSquare {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double perimeter = input.nextDouble();
+
+        double side = perimeter / 4;
+
+        System.out.println("The length of the side is " + side +
+                " whose perimeter is " + perimeter);
+    }
+}
