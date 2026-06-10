838bec6d1548851067d8dd83eaa7c1b01efc3b36
Hemant-Kumar <hemantkushwaha2405@gmail.com>
2026-06-10 13:30:01 +0530

    [Hemant] Added: Scenerio Based Questions

--- core-java-practice/leetcode-codebase/SubtractTheProductAndSum.java
+++ core-java-practice/leetcode-codebase/SubtractTheProductAndSum.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
-
 public class SubtractTheProductAndSum {
      public static int subtractProductAndSum(int n) {
         int sum=0;
         int prod=1;
         while(n!=0){
             int rem=n%10;
-
             sum+=rem;
             prod*=rem;
             n=n/10;
-
         }
         return prod-sum;
-
     }
      public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
