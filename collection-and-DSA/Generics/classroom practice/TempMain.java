c6099590e0b3ccb7906b6e1f9f990df2ad944d1a
Hemant-kumar24 <hemantkushwaha2405@gmail.com>
2026-07-15 12:25:38 +0530

    Revert "Collection and dsa"

--- collection-and-DSA/Generics/classroom practice/TempMain.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-class TempMain {
-    public static void main(String[] args) {
-        List<Flight<Integer>> flights = new ArrayList<>();
-        flights.add(new Flight<>(101, "NYC", "LAX"));
-        FlightUtility.displayFlightDetails(flights);
-    }
-}
